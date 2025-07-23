c57bfaf83233754ef43ed3659b1da2140d52ec6e
amitsaini1912 <amitkumarsainig9@gmail.com>
2025-07-23 18:39:59 +0530

    PrivateComponets component created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import SignUp from './components/signup';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import AddProduct from './components/addProduct';
+import PrivateComponent from './components/privateComponent';
 
 function App() {
   return (
@@ -10,11 +11,17 @@ function App() {
       <BrowserRouter>
         <h1>reWear</h1>
          <Routes>
-           <Route path='/' element={<h1>Product List Page</h1>} />
-           <Route path='/add-product' element={<AddProduct/>} />
-           <Route path='/profile' element={<h1>Profile Page</h1>} />
+
+           <Route element={<PrivateComponent/>} >
+              <Route path='/' element={<h1>Product List Page</h1>} />
+              <Route path='/add-product' element={<AddProduct/>} />
+              <Route path='/profile' element={<h1>Profile Page</h1>} />
+            
+              <Route path='/logout' element={<h1>Log Out Page</h1>} />
+           </Route>
+
            <Route path='/signup' element={<SignUp/>} />
-           <Route path='/logout' element={<h1>Log Out Page</h1>} />
+
          </Routes>
       </BrowserRouter>
     </div>
